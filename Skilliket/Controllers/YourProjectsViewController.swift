2337dd666a5b970da16467b591a212d6eccaad15
Wilfrido TA <a01664769@tec.mx>
2024-10-12 17:01:09 -0600

    Funcionalidad y identificadores
    Se han agregado los identificadores correspondientes a cada celda; se le ha dado funcionalidad a una celda de projects para pruebas con la pantalla que abre

--- Skilliket/Controllers/YourProjectsViewController.swift
+++ Skilliket/Controllers/YourProjectsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class YourProjectsViewController: UIViewController {
+class YourProjectsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var yourProjectsTable: UITableView!
     
@@ -15,7 +15,36 @@ class YourProjectsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Cargar info en la tabla
+        yourProjectsTable.dataSource = self//La misma clase será encargada de obtener la información de la celda
+        yourProjectsTable.delegate = self//Tamaño de celda - UITableViewDelegate
+    }
+    
+    //MARK: - Funciones de la tabla
+    //El tamaño del arreglo nos dira el numero de secciones
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return communitiesArr.count
+    }
+    
+    //Cada sección tendra una fila
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = yourProjectsTable.dequeueReusableCell(withIdentifier: "UserProjectCell", for: indexPath) as! YourProjectsTableViewCell
+        
+        let arreglo = communitiesArr[indexPath.row]
+        
+        cell.yourProjectsName.text = arreglo.name
+        
+        return cell
+    }
+    
+    //Mantener constante el tamaño de cada celda
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250 //150 Será el tamaño para todas las tablas
     }
     
 
