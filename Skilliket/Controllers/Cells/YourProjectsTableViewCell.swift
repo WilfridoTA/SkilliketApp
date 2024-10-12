cce4f163793c65cc70a933e01083b45dcbef185b
Wilfrido TA <a01664769@tec.mx>
2024-10-12 16:30:12 -0600

    Your Projects - Actualizacion
    Se crearon los controllers para el view y cells de "Your Projects"; se agrega nuevo view controller para mostrar contenido del proyecto

---
+++ Skilliket/Controllers/Cells/YourProjectsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  YourProjectsTableViewCell.swift
+//  Skilliket
+//
+//  Created by Will on 12/10/24.
+//
+
+import UIKit
+
+class YourProjectsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var YourProjectsImage: UIImageView!
+    @IBOutlet weak var yourProjectsName: UILabel!
+    @IBOutlet weak var yourProjectsDescription: UILabel!
+    
+
+}
