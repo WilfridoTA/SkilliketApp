1eb77d98b2921030696205d1fe4ba192c6738532
Wilfrido TA <a01664769@tec.mx>
2024-10-12 22:53:28 -0600

    Announcements Terminado
    Se ha terminado la pantalla en storyboard; Se agrega el table cell controller con los elementos de la celda; se agrega el view controller de la pantalla

---
+++ Skilliket/Controllers/Cells/ProjectAnnouncementTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectAnnouncementTableViewCell.swift
+//  Skilliket
+//
+//  Created by Will on 12/10/24.
+//
+
+import UIKit
+
+class ProjectAnnouncementTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var projectAnnouncementImage: UIImageView!
+    @IBOutlet weak var projectAnnouncementUserName: UILabel!
+    @IBOutlet weak var projectAnnouncementName: UILabel!
+    @IBOutlet weak var projectAnnouncementDescription: UILabel!
+    @IBOutlet weak var projectAnnouncementDate: UILabel!
+    
+    //Los botos de like y dislike no estan agregados
+    
+}
