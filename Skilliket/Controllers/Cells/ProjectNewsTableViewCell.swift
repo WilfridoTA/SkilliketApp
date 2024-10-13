42d31cc9c3715c3c2126f0e1d88775e630f695ac
Wilfrido TA <a01664769@tec.mx>
2024-10-12 22:59:57 -0600

    Pantalla Project News
    Se agrega el view controller para la pantalla "news"; se agrega el identificado de celda; se agregan los elementos de la celda en el table view cell

---
+++ Skilliket/Controllers/Cells/ProjectNewsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectNewsTableViewCell.swift
+//  Skilliket
+//
+//  Created by Will on 12/10/24.
+//
+
+import UIKit
+
+class ProjectNewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var ProjectNewsImage: UIImageView!
+    @IBOutlet weak var projectNewsTitulo: UILabel!
+    @IBOutlet weak var projectNewsDate: UILabel!
+    
+}
