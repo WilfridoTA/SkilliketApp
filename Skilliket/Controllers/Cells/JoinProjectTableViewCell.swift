6da72bee3b9b76845e9d6f85d909fe73a0adfc46
Wilfrido TA <a01664769@tec.mx>
2024-10-14 23:38:16 -0600

    Join Project creado
    Se creo la pantalla en storyboard; se crea la celda; se genera view controller y table view cell; se cambia el texto de la tab bar de return a "log out"

---
+++ Skilliket/Controllers/Cells/JoinProjectTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  JoinProjectTableViewCell.swift
+//  Skilliket
+//
+//  Created by Will on 14/10/24.
+//
+
+import UIKit
+
+class JoinProjectTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var joinProjectImage: UIImageView!
+    @IBOutlet weak var jointProjectName: UILabel!
+    @IBOutlet weak var joinProjectDescription: UILabel!
+    
+
+}
---
+++ Skilliket/Controllers/JoinProjectViewController.swift
@@ -0,0 +1,31 @@
+//
+//  JoinProjectViewController.swift
+//  Skilliket
+//
+//  Created by Will on 14/10/24.
+//
+
+import UIKit
+
+class JoinProjectViewController: UIViewController {
+    
+    @IBOutlet weak var joinProjectTable: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
