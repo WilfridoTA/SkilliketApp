34025d11730f6e61020f4b04bbe37878f5588236
LeriAx75 <a01663303@tec.mx>
2024-10-15 18:08:18 -0600

    A2
    agregue funcionalidad para el teclado

--- Skilliket/Controllers/CreateAccountVC.swift
+++ Skilliket/Controllers/CreateAccountVC.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class CreateAccountVC: UIViewController {
+class CreateAccountVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var scrollView: UIScrollView!
     var password:String?
     var ourApp:App?
     var name:String?
@@ -30,6 +31,16 @@ class CreateAccountVC: UIViewController {
         // Do any additional setup after loading the view.
         addMargin(button: agreeTermsButton)
         addMargin(button: eulaButton)
+        registerForKeyboardNotifications()
+        self.passwordTextField.delegate=self
+        self.emailTextField.delegate=self
+        self.usernameTextField.delegate=self
+        self.lastNameTextField.delegate=self
+        self.nameTextField.delegate=self
+        
+        hideKeyboardWhenTappedAround()
+        
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -185,5 +196,35 @@ class CreateAccountVC: UIViewController {
             destinatiomVC.ourApp=ourApp
         }
     }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWasShown(_ notification: NSNotification) {
+        guard let info = notification.userInfo,
+            let keyboardFrameValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else { return }
+
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+    }
+
+    @objc func keyboardWillBeHidden(_ notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+    }
+    
+    //MARK: - Cerrar tecladodo
+    //Cerrar el teclado al presionar return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() //Aplicar para todos los TextField
+    }
+    
 
 }
+
+
---
+++ Skilliket/Controllers/CreateProjectUserViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateProjectUserViewController.swift
+//  Skilliket
+//
+//  Created by Astrea Polaris on 15/10/24.
+//
+
+import UIKit
+
+class CreateProjectUserViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        hideKeyboardWhenTappedAround()
+    }
+    
+
+    //MARK: - Cerrar tecladodo
+    //Cerrar el teclado al presionar return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() //Aplicar para todos los TextField
+    }
+    
+
+}
