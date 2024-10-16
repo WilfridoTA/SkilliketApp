d3675cd5506cd5ff04eb80716067b75f91d0c46d
LeriAx75 <a01663303@tec.mx>
2024-10-15 23:17:25 -0600

    ya ni se

---
+++ SkilliketTests/SkilliketTests.swift
@@ -0,0 +1,16 @@
+//
+//  SkilliketTests.swift
+//  SkilliketTests
+//
+//  Created by Astrea Polaris on 15/10/24.
+//
+
+import Testing
+
+struct SkilliketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
