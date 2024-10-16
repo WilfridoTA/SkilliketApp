5caffe08751086b9f2900262c76a3253e7c74cd5
Wilfrido TA <a01664769@tec.mx>
2024-10-16 11:51:33 -0600

    commit "ZZZZ"
    Se agrega el commit de Arely de la rama 4 - "correji announcments de project"

--- Skilliket/Controllers/ProjectAnnouncementViewController.swift
+++ Skilliket/Controllers/ProjectAnnouncementViewController.swift
@@ -31,23 +31,57 @@ class ProjectAnnouncementViewController: UIViewController, UITableViewDataSource
     
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return announcmentsArr!.count
     }
     
     //Cada sección tendra una fila
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return announcmentsArr!.count
+        return 1
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = announcementTable.dequeueReusableCell(withIdentifier: "projectNewsCell", for: indexPath) as! ProjectAnnouncementTableViewCell
+        let cell = announcementTable.dequeueReusableCell(withIdentifier: "announcementCell", for: indexPath) as! ProjectAnnouncementTableViewCell
         
-        let arreglo = announcmentsArr![indexPath.row]
+        let arreglo = announcmentsArr![indexPath.section]
         
-        cell.projectAnnouncementDate.text = "\(arreglo.dateCreated.day)/\(arreglo.dateCreated.month)/\(arreglo.dateCreated.year)"
+        cell.projectAnnouncementDescription.text=arreglo.text
+                cell.projectAnnouncementUserName.text=arreglo.creator
+                cell.projectAnnouncementDate.text = "\(arreglo.dateCreated.day!)/\(arreglo.dateCreated.month!)/\(arreglo.dateCreated.year!)"
+                if let im=getMember(name: arreglo.creator)?.profileImage{
+                    cargarImagenDesdeURL(url: im, imageView: cell.projectAnnouncementImage)
+                }
         
         return cell
     }
+    
+    func cargarImagenDesdeURL(url: URL, imageView: UIImageView) {
+           URLSession.shared.dataTask(with: url) { (data, response, error) in
+           guard let data = data, error == nil else {
+               print("Error al cargar la imagen: \(error?.localizedDescription ?? "Sin descripción de error")")
+               return
+           }
+                                                
+           DispatchQueue.main.async {
+               if let image = UIImage(data: data) {
+                   imageView.image = image
+               } else {
+                   print("No se pudo crear la imagen")
+               }
+           }
+           }.resume()
+       }
+       
+       func getMember(name:String)->Member?{
+           //te da el objeto Member que tiene el nombre ingresado
+           var member:Member?=nil
+           for i in 0...ourApp!.userMembers.count-1{
+               let m=ourApp!.userMembers[i]
+               if m.name==name{
+                   member=m
+               }
+           }
+           return member
+       }
 
 }
