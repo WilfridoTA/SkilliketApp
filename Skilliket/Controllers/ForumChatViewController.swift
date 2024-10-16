5ff12831426dbec1c7d7c3d75db03fb14a74dc57
LeriAx75 <a01663303@tec.mx>
2024-10-16 08:37:02 -0600

    good morning
    cambios al chat

---
+++ Skilliket/Controllers/ForumChatViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ForumChatViewController.swift
+//  Skilliket
+//
+//  Created by Astrea Polaris on 16/10/24.
+//
+
+import UIKit
+
+class ForumChatViewController: UIViewController {
+
+    @IBOutlet var insertText: UITextField!
+    @IBOutlet var table: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
