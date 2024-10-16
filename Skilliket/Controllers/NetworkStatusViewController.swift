64f467a094b89cf1cfa6b371712035951d4969cd
LeriAx75 <a01663303@tec.mx>
2024-10-16 13:33:21 -0600

    huhfeiwf
    funcionalidad network

--- Skilliket/Controllers/NetworkStatusViewController.swift
+++ Skilliket/Controllers/NetworkStatusViewController.swift
@@ -12,6 +12,14 @@ class NetworkStatusViewController: UIViewController {
     var actualAdmin:Admin?
     var networkStatuses:Statuses?
 
+    @IBOutlet var totalPercentage: UILabel!
+    @IBOutlet var totalDevices: UILabel!
+    @IBOutlet var switchRatio: UILabel!
+    @IBOutlet var switchPercentage: UILabel!
+    @IBOutlet var routerRatio: UILabel!
+    @IBOutlet var routerPercentage: UILabel!
+    @IBOutlet var clientsPercentage: UILabel!
+    @IBOutlet var clientsTotal: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         Task {
@@ -21,5 +29,32 @@ class NetworkStatusViewController: UIViewController {
             }
         }
         
+    }
+    
+    func setUp(){
+        let actualStatuus=networkStatuses![networkStatuses!.count-1]
+        totalPercentage.text=actualStatuus.networkDevices.totalPercentage
+        totalDevices.text=actualStatuus.networkDevices.totalDevices
+        var Switch:[NetworkDevice?]=[]
+        for i in 0...actualStatuus.networkDevices.networkDevices.count-1{
+            if actualStatuus.networkDevices.networkDevices[i].deviceType == .switches{
+                Switch.append(actualStatuus.networkDevices.networkDevices[i])
+            }
+        }
+        switchRatio.text=Switch[0]!.healthyRatio
+        switchPercentage.text=Switch[0]!.healthyPercentage
+        
+        var router:[NetworkDevice?]=[]
+        for i in 0...actualStatuus.networkDevices.networkDevices.count-1{
+            if actualStatuus.networkDevices.networkDevices[i].deviceType == .routers{
+                router.append(actualStatuus.networkDevices.networkDevices[i])
+            }
+        }
+        
+        routerRatio.text=router[0]!.healthyRatio
+        routerPercentage.text=router[0]!.healthyPercentage
+        
+        clientsTotal.text=actualStatuus.clients.totalConnected
+        clientsPercentage.text=actualStatuus.clients.totalPercentage
     }
 }
